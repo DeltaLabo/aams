8fab3c99025e6f9052cdfc94ae1dfac86b92bd0a
Nicole P.M <115262250+nicolepradom@users.noreply.github.com>
2024-04-29 20:29:58 -0600

    Update main.io w/ SD and OPC

--- main/main.ino
+++ main/main.ino
@@ -1,109 +1,274 @@
-// Codigo Proyecto AAMS - Laboratorio Delta
+// Code AAMS Project - DELTA Lab
 #include <WiFi.h>
 #include "ThingSpeak.h"
 #include <DHT.h>
-#define DHT22_PIN  6 // ESP32 pin GPIO23 connected to DHT22 sensor
+#include <SPI.h>
 
-DHT dht22(DHT22_PIN, DHT22);
+//Libraries for OPC
+#define ArduinoUNO
+#define opSerial Serial
+#define BaudRate 115200
+#define SPI_OPC_busy 0x31
+#define SPI_OPC_ready 0xF3
 
-//const char* ssid = "LaboratorioDelta";   // your network SSID (name) 
-//const char* pass = "labdelta21!";        // your network password
-const char* ssid = "JMR";   // your network SSID (name) 
-const char* pass = "JDMR0106";        // your network password
+//Libraries for SD Card
+#include <BufferedPrint.h>
+#include <FreeStack.h>
+#include <MinimumSerial.h>
+#include <RingBuf.h>
+#include <SdFat.h>
+#include <SdFatConfig.h>
+#include <sdios.h>
+
+//Libraries for RTC Module
+#include <RTClib.h>
+#include <Wire.h>
+
+const char* ssid = "LaboratorioDelta";   // your network SSID (name) 
+const char* pass = "labdelta21!";        // your network password
+//const char* ssid = "HONOR JOSEPH";   // your network SSID (name) 
+//const char* pass = "123456789";        // your network password
 int keyIndex = 0;                 // your network key Index number (needed only for WEP)
 WiFiClient  client;
 
 unsigned long myChannelNumber = 2363549;
 const char * myWriteAPIKey = "NR8GHTJF3IC27CIP";
 
-//Codigo para leer sensores
-//Para ESP32
+// define pin for tempertaure and humidity sensor
+#define DHT22_PIN  6
+#define DHTTYPE DHT22
+DHT dht(DHT22_PIN, DHTTYPE, 22);
+float humi = 0;
+float tempC = 0;
 
+// define custom SPI pins
+#define PIN_MISO 45 //DO
+#define PIN_MOSI 46 //DI
+#define PIN_SCK 47
+#define PIN_CS 26  // chip select pin for SD card
+
+RTC_DS3231 rtc;
+SdFat SD;
+SdFile myFile;
+char filename[13]; // Buffer for filename with room for null terminator
+
+// initialize custom SPI class for SD card
+SPIClass sdSPI(HSPI);
+
+// pins for sensors
 int CO_pin = 7;
 int NO_pin = 5;
 int SO_pin = 3;
 
-//Variables to store the value coming from the sensor
+// variables to store the value coming from the sensor
 float CO_value = 0;
 float NO_value = 0;
 float SO_value = 0;
+float PM1 = 0;
+float PM2 = 0;
+float PM10 = 0;
 //Counter to update values on Things Speak
 int segundos = 0;
 
-void setup() {
+unsigned long currentTime;
+unsigned long cloopTime;
+unsigned char SPI_in[68], SPI_in_index, ssPin_OPC;
+
+struct SEND_DATA_STRUCTURE{
+  unsigned int hist[16];
+  float temp;
+  float humid;
+  float PM[3];
+};
+SEND_DATA_STRUCTURE mydata;
+
+void setup()
+{
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);   
   ThingSpeak.begin(client);
-  dht22.begin(); // initialize the DHT22 sensor
+  dht.begin(); // initialize the DHT22 sensor
+  Wire.begin(41,42); // initialize I2C for RTC (GPIO41 for SDA and GPIO42 for SCL)
+
+  //Set all the pins available for use as SS pins to outputs and set HIGH
+  for (unsigned char i=2;i<11;i++)
+  {
+    digitalWrite(i, HIGH); //Initiate pin HIGH
+    pinMode(i, OUTPUT); //Set pin as output
+  }
+
+  delay(1000); //delay in case of noise on power connection. Also allows OPC to boot up.
+
+  //Start serial port
+  opSerial.begin(BaudRate);
+
+  // start the SPI library:
+  SPI.begin(); //Enable SPI for OPC comms
+
+  //Device #1 (ssPin_OPC = 10)
+  ssPin_OPC = 33;
+  InitDevice();
+  //END Device #1
+  delay(1000);
+  PrintDataLabels(opSerial); //Print labels to serial port - optional BL
+
+  // setup RTC module
+  if (!rtc.begin()) {
+    Serial.println(F("Couldn't find RTC"));
+    while (1);
+  }
+  // adjust time in RTC module to now everytime
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  // initialize SPI for SD card on custom pins
+  sdSPI.begin(PIN_SCK, PIN_MISO, PIN_MOSI, PIN_CS); // SCK, MISO, MOSI, SS
+
+  // inicialize SD Card
+  if (!SD.begin(SdSpiConfig(PIN_CS, SHARED_SPI, SD_SCK_MHZ(50), &sdSPI))) {
+    Serial.println(F("SD CARD FAILED, OR NOT PRESENT!"));
+    //while (1);  // Do nothing more if SD card fails
+  } else{
+    Serial.println(F("SD CARD INITIALIZED."));
+    Serial.println(F("--------------------"));    
+  }
 }
 
-float gasSensor(int pin, int offset, int sens) {
-  // float value = (analogRead(pin)*2560.0)/1023.0;
-  float valor = (analogRead(pin)*3300.0)/4095.0;
-  Serial.println(analogRead(pin));
-  Serial.println("Voltaje del pin: " + String(pin) + " Es de: " + String(valor));
-  valor -= offset; //substract offset
-  valor /= sens; //apply sensitivity
-  valor *= 1000; //convert to ppb
-  return valor;
+void updateFilename(char* filename, const DateTime& dt) {
+  sprintf(filename, "/%04d%02d%02d.txt", dt.year(), dt.month(), dt.day());
 }
 
-void loop() {
+void InitDevice (void){
+
+  ReadOPCstring(0x10); //Get serialstr from OPC device
+  ReadOPCstring(0x3F); //Get infostr from OPC device
+
+  StartOPC(); //Switch on power to fan and laser
+  
+  //ReadOPCconfig(opSerial); //Get Config data (bin boundaries etc.) from OPC device
+}
+
+void CollectData(){ 
+  Serial.println("\nNUEVA LECTURA.");
+
+  // read data from DHT22
+  humi  = dht.readHumidity();    // read humidity
+  tempC = dht.readTemperature(); // read temperature
+  // check if the reading is successful or not
+  if (isnan(tempC) || isnan(humi)) {
+    Serial.println("ERROR");
+  } 
+  else {
+    // display the temperature
+    Serial.print("Temp: ");
+    Serial.print(tempC); 
+    Serial.println("°C");
+
+    // display the humidity
+    Serial.print("Humidity: ");
+    Serial.print(humi);      
+    Serial.println("%");
+  }
+  
+  // diaplay sensor values
+  CO_value = CO_value + gasSensor(CO_pin,280,430);
+  NO_value = NO_value + gasSensor(NO_pin,205,400);
+  SO_value = SO_value + gasSensor(SO_pin,345,520);
+  delay(1000);
+  Serial.print("CO concentration: ");
+  Serial.print(CO_value/(segundos+1));
+  Serial.println(" ppb");
+  Serial.print("NO2 concentration: ");
+  Serial.print(NO_value/(segundos+1));
+  Serial.println(" ppb");
+  Serial.print("SO2 concentration: ");
+  Serial.print(SO_value/(segundos+1));
+  Serial.println(" ppb");
+
+  ssPin_OPC = 33;
+  unsigned long GetHistTime = millis(); //Set initial GetHistTime
+  ReadOPChist(); //Read OPC histogram data
+  opSerial.print(millis());
+  PrintData(opSerial); //Print data to serial
+
+  //display PMs
+  PM1 = mydata.PM[0];
+  Serial.print("PM1: ");
+  //Serial.print(mydata.PM[0]);
+  Serial.print(PM1);    
+  Serial.println("ug/m^3");
+  
+  PM2 = mydata.PM[1];
+  Serial.print("PM2.5: ");
+  //Serial.print(mydata.PM[1]);
+  Serial.print(PM2);
+  Serial.println("ug/m^3");
+  
+  PM10 = mydata.PM[2];
+  Serial.print("PM10: ");
+  //Serial.print(mydata.PM[2]);
+  Serial.print(PM10);
+  Serial.println("ug/m^3");
+  
+  delay(10000);
+
+  if (segundos <= 30){
+  segundos = segundos + 1;
+  }
+  else{
+  WriteDataIoT();
+  segundos = 0;
+  CO_value = 0;
+  NO_value = 0;
+  SO_value = 0;
+  tempC = 0;
+  humi = 0;
+  PM1 = 0;
+  PM2 = 0;
+  PM10 = 0;
+  }
+}
+
+// Main Loop
+void loop()
+{
   // Connect or reconnect to WiFi
   if(WiFi.status() != WL_CONNECTED){
-    Serial.print("Attempting to connect to SSID: ");
+    Serial.print("Could not connect to:");
     Serial.println(ssid);
     while(WiFi.status() != WL_CONNECTED){
-      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
-      Serial.print(".");
-      delay(5000);     
-    } 
-    Serial.println("\nConnected.");
-  } 
-  else{
-    //Calculate the value of each variable and prints them
-    float humi  = dht22.readHumidity();    // read humidity
-    float tempC = dht22.readTemperature(); // read temperature
-    // check if the reading is successful or not
-    if (isnan(tempC) || isnan(humi)) {
-      Serial.println("ERROR");
-    } 
-    else {
-      // display the temperature
-      Serial.print(tempC);     
-      Serial.print("°C");
-      Serial.print("Temp: ");
+      WiFi.begin(ssid, pass);
+      delay (5000);
 
-      // display the humidity
-      Serial.print("Humidity: ");
-      Serial.print(humi);      
-      Serial.println("%");
-    }
-    Serial.println("\nNUEVA LECTURA.");
-    CO_value = CO_value + gasSensor(CO_pin,280,445);
-    NO_value = NO_value + gasSensor(NO_pin,203,740);
-    SO_value = SO_value + gasSensor(SO_pin,350,307);
-    delay(1000);
-    Serial.print("CO concentration: ");
-    Serial.print(CO_value/(segundos+1));
-    Serial.println(" ppb");
-    Serial.print("NO2 concentration: ");
-    Serial.print(NO_value/(segundos+1));
-    Serial.println(" ppb");
-    Serial.print("SO2 concentration: ");
-    Serial.print(SO_value/(segundos+1));
-    Serial.println(" ppb");
-    if (segundos <= 30){
-      segundos = segundos + 1;
-    } 
-    else{
-      WriteDataIoT();
+      // if WiFi fails, use SD card
+      CollectData();
+      DataLogSD();
       segundos = 0;
       CO_value = 0;
       NO_value = 0;
       SO_value = 0;
+      tempC = 0;
+      humi = 0;
+      PM1 = 0;
+      PM2 = 0;
+      PM10 = 0;
     }
+    Serial.println("\nConnected.");
+  } 
+  else{
+    CollectData();
   }
+}
+
+
+float gasSensor(int pin, int offset, int sens) {
+  // float value = (analogRead(pin)*2560.0)/1023.0;
+  float valor = (analogRead(pin)*5000.0)/4095.0;
+  //Serial.println(analogRead(pin));
+  //Serial.println("Voltaje del pin: " + String(pin) + " Es de: " + String(valor));
+  valor -= offset; //substract offset
+  valor /= sens; //apply sensitivity
+  valor *= 1000; //convert to ppb
+  return valor;
 }
 
 void WriteDataIoT() {
@@ -111,6 +276,11 @@ void WriteDataIoT() {
   ThingSpeak.setField(1, (CO_value/(segundos+1)));
   ThingSpeak.setField(2, (NO_value/(segundos+1)));
   ThingSpeak.setField(3, (SO_value/(segundos+1)));
+  ThingSpeak.setField(4, (tempC));
+  ThingSpeak.setField(5, (humi));
+  ThingSpeak.setField(6, (PM1));
+  ThingSpeak.setField(7, (PM2));
+  ThingSpeak.setField(8, (PM10));
   
   // write to the ThingSpeak channel
   int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
@@ -121,4 +291,355 @@ void WriteDataIoT() {
     Serial.println("Problem updating channel. HTTP error code " + String(x));
   }
   return;
+}
+
+
+//Get string (serialstr or infostr) from OPC device
+void ReadOPCstring (unsigned char SPIcommand)
+{
+  GetReadyResponse(SPIcommand);
+  for (SPI_in_index=0; SPI_in_index<60; SPI_in_index++)
+  {
+    delayMicroseconds(10);
+    SPI_in[SPI_in_index] = SPI.transfer(0x01); //Value of outgoing byte doesn't matter
+  }
+
+  SetSSpin(HIGH);
+  SPI.endTransaction();
+
+  PrintOPCstring(opSerial);
+}
+
+void PrintOPCstring (Stream &port)
+{
+  port.write(SPI_in, 60); //print 60 characters from SPI_in[] array
+  port.println("");
+  port.flush();
+}
+
+void ReadOPChist (void)
+{
+  GetReadyResponse(0x30);
+  for (SPI_in_index=0; SPI_in_index<64; SPI_in_index++)
+  {
+    delayMicroseconds(10);
+    SPI_in[SPI_in_index] = SPI.transfer(0x01); //Value of outgoing byte doesn't matter
+  }
+  SetSSpin(HIGH);
+  SPI.endTransaction();
+  delay(10);
+}
+
+
+void DiscardSPIbytes (byte NumToDiscard)
+{
+  for (SPI_in_index=0; SPI_in_index<NumToDiscard; SPI_in_index++)
+  {
+    delayMicroseconds(10);
+    SPI.transfer(0x01); //Value of outgoing byte doesn't matter
+  }
+}
+
+
+void StartOPC (void)
+{
+  //Turn ON fan and peripheral power
+  GetReadyResponse(0x03);
+  SPI.transfer(0x03); //Turn ON fan and peripheral power
+  SetSSpin(HIGH);
+  SPI.endTransaction();
+  delay(10);
+
+  //Wait for fan to reach full speed (and for multiple attempts by OPC firmware to turn on fan)
+  for (byte i=0; i<5; i++)
+  {
+    delay(1000);
+  }
+}
+
+
+void GetReadyResponse (unsigned char SPIcommand)
+{
+  unsigned char Response;
+
+  SPI.beginTransaction(SPISettings(300000, MSBFIRST, SPI_MODE1));
+
+  //Try reading a byte here to clear out anything remnant of SD card SPI activity (WORKS!)
+  Response = SPI.transfer(SPIcommand);
+  delay(1);  //wait 1ms
+
+  do
+  {
+    SetSSpin(LOW);
+    unsigned char Tries = 0;
+    do
+    {
+      Response = SPI.transfer(SPIcommand);
+      if (Response != SPI_OPC_ready) delay(1); //wait 1ms
+    }
+    while ((Tries++ < 20) && (Response != SPI_OPC_ready));
+
+    if (Response != SPI_OPC_ready)
+    {
+      if (Response == SPI_OPC_busy)
+      {
+        SetSSpin(HIGH);
+        Serial.println(F("ERROR Waiting 2s (for OPC comms timeout)")); //signal user
+        Serial.flush();
+        delay(2000); //wait 2s
+      }
+      else
+      {
+        //End SPI and wait a few seconds for it to be cleared
+        SetSSpin(HIGH);
+        Serial.println(F("ERROR Resetting SPI")); //signal user
+        Serial.flush();
+        SPI.endTransaction();
+        //Wait 6s here for buffer to be cleared
+        delay(6000);
+        SPI.beginTransaction(SPISettings(300000, MSBFIRST, SPI_MODE1));
+      }
+    }
+  }
+  while ((Response != SPI_OPC_ready) && (Serial.available()==0)); //don't hang on this if data is coming in on serial interface
+  delay(10);
+
+}
+
+
+unsigned int MODBUS_CalcCRC(unsigned char data[], unsigned char nbrOfBytes)
+{
+  #define POLYNOMIAL_MODBUS 0xA001 //Generator polynomial for MODBUS crc
+  #define InitCRCval_MODBUS 0xFFFF //Initial CRC value
+
+  unsigned char _bit; // bit mask
+  unsigned int crc = InitCRCval_MODBUS; // initialise calculated checksum
+  unsigned char byteCtr; // byte counter
+
+  // calculates 16-Bit checksum with given polynomial
+  for(byteCtr = 0; byteCtr < nbrOfBytes; byteCtr++)
+  {
+    crc ^= (unsigned int)data[byteCtr];
+    for(_bit = 0; _bit < 8; _bit++)
+    {
+      if (crc & 1) //if bit0 of crc is 1
+      {
+        crc >>= 1;
+        crc ^= POLYNOMIAL_MODBUS;
+      }
+      else
+        crc >>= 1;
+    }
+  }
+  return crc;
+}
+
+
+//Convert SHT31 ST output to Temperature (C)
+float ConvSTtoTemperature (unsigned int ST)
+{
+  return -45 + 175*(float)ST/65535;
+}
+
+
+//Convert SHT31 SRH output to Relative Humidity (%)
+float ConvSRHtoRelativeHumidity (unsigned int SRH)
+{
+  return 100*(float)SRH/65535;
+}
+
+
+//Process OPC data and print
+void PrintData (Stream &port)
+{
+  unsigned char i;
+  unsigned int *pUInt16;
+  float *pFloat;
+  float Afloat;
+  int k;
+
+  //Histogram bins (UInt16) x16
+  k=0;
+  for (i=0; i<32; i+=2)
+  {
+    AddDelimiter(port);
+    pUInt16 = (unsigned int *)&SPI_in[i];
+    port.print(*pUInt16, DEC);
+    mydata.hist[k] = *pUInt16;
+    k++;
+  }
+
+  //MToF bytes (UInt8) x4
+  for (i=32; i<36; i++)
+  {
+    AddDelimiter(port);
+    Afloat = (float)SPI_in[i];
+    Afloat /= 3; //convert to us
+    port.print(Afloat, 2);
+  }
+
+  //SFR (4-byte float) x1
+  AddDelimiter(port);
+  pFloat = (float *)&SPI_in[36];
+  port.print(*pFloat, 3); //print to 3dp
+
+  //Temperature (2-byte integer) x1
+  AddDelimiter(port);
+  pUInt16 = (unsigned int *)&SPI_in[40];
+  port.print(ConvSTtoTemperature(*pUInt16), 1); //print to 1dp
+  mydata.temp = ConvSTtoTemperature(*pUInt16);
+
+  //Relative humidity (2-byte integer) x1
+  AddDelimiter(port);
+  pUInt16 = (unsigned int *)&SPI_in[42];
+  port.print(ConvSRHtoRelativeHumidity(*pUInt16), 1); //print to 1dp
+  mydata.humid = ConvSRHtoRelativeHumidity(*pUInt16);
+
+  //Sampling period(s) (4-byte float) x1
+  AddDelimiter(port);
+  pFloat = (float *)&SPI_in[44];
+  port.print(*pFloat, 3); //print to 3dp
+
+  //Reject count Glitch (1-byte integer) x1
+  AddDelimiter(port);
+  port.print(SPI_in[48], DEC);
+
+  //Reject count LongTOF (1-byte integer) x1
+  AddDelimiter(port);
+  port.print(SPI_in[49], DEC);
+
+  //PM values(ug/m^3) (4-byte float) x3
+  k = 0;
+  for (i=50; i<62; i+=4)
+  {
+    AddDelimiter(port);
+    pFloat = (float *)&SPI_in[i];
+    port.print(*pFloat, 3); //print to 3dp
+    mydata.PM[k] = *pFloat;
+    k++;
+  }
+
+  //Checksum (UInt16) x1
+  AddDelimiter(port);
+  pUInt16 = (unsigned int *)&SPI_in[62];
+  port.println(*pUInt16, DEC);
+
+  //Compare recalculated Checksum with one sent
+  if (*pUInt16 != MODBUS_CalcCRC(SPI_in, 62)) //if checksums aren't equal
+    port.println(F("Checksum error in line above!"));
+
+  port.flush();
+}
+
+
+//Print data labels
+void PrintDataLabels (Stream &port)
+{
+  unsigned char i;
+
+  port.print(F("Time(ms)"));
+
+  for (i=0; i<16; i++)
+  {
+    port.print(F(",Bin"));
+    if (i < 10) port.print(F("0")); //leading 0 for single digit bin numbers
+    port.print(i, DEC);
+  }
+
+  for (i=1; i<9; i+=2)
+  {
+    port.print(F(",MToFBin"));
+    port.print(i, DEC);
+    if (i == 1) port.print(F("(us)")); //print units for first value of this type
+  }
+
+  port.println(F(",SFR(ml/s),T(C),RH(%),SampPrd(s),#RejectGlitch,#RejectLong,PM_A(ug/m^3),PM_B,PM_C,Checksum"));
+
+  port.flush();
+}
+
+void AddDelimiter (Stream &port)
+{
+  port.print(F(",")); //delimiter
+}
+
+void SetSSpin (bool pinState) //pinState is HIGH or LOW
+{
+  digitalWrite(ssPin_OPC, pinState); //Set output to pinState
+}
+
+void DataLogSD(){
+  // set filename and state date
+  DateTime now = rtc.now();
+  updateFilename(filename, now);  // Update filename based on the current date;
+
+  // open file for writing
+  if (!myFile.open(filename, O_RDWR | O_CREAT | O_AT_END)) {
+    Serial.print(F("SD Card: error on opening file "));
+    Serial.println(filename);
+  }
+  else{
+    Serial.println(F("Writing log to SD Card"));
+
+    // write timestamp
+    myFile.print(now.year(), DEC);
+    myFile.print('-');
+    myFile.print(now.month(), DEC);
+    myFile.print('-');
+    myFile.print(now.day(), DEC);
+    myFile.print(' ');
+    myFile.print(now.hour(), DEC);
+    myFile.print(':');
+    myFile.print(now.minute(), DEC);
+    myFile.print(':');
+    myFile.print(now.second(), DEC);
+    myFile.print("  "); // Delimiter between timestamp and data
+
+    // write data
+    myFile.println();
+
+    // CO sensor data
+    myFile.print("CO concentration: ");
+    myFile.print(CO_value/(segundos+1));
+    myFile.println(" ppb");
+    //myFile.print(", "); //Delimiter between data
+
+    // NO2 sensor data
+    myFile.print("NO2 concentration: ");
+    myFile.print(NO_value/(segundos+1));
+    myFile.println(" ppb");
+
+    // SO2 sensor data
+    myFile.print("SO2 concentration: ");
+    myFile.print(SO_value/(segundos+1));
+    myFile.println(" ppb");
+
+    // temperature sensor data
+    myFile.print("Temperature: ");
+    myFile.print(tempC);
+    myFile.println("°C");
+
+    // humidity sensor data
+    myFile.print("Humidity: ");
+    myFile.print(humi);
+    myFile.println("%");
+
+    // OPC PM data
+    myFile.print("PM1: ");
+    myFile.print(PM1);      
+    myFile.println("ug/m^3");
+
+    myFile.print("PM2.5: ");
+    myFile.print(PM2);      
+    myFile.println("ug/m^3");
+
+    myFile.print("PM10: ");
+    myFile.print(PM10);
+    myFile.println("ug/m^3");
+
+    myFile.println();  // New line
+
+    myFile.close();  // Close the file
+    Serial.println("Data appended to file.");
+  }
 }
